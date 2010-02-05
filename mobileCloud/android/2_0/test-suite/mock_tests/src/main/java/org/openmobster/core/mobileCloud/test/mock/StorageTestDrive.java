9679e0df6eea63abc76a9183ee66e899feff9733
openmobster <openmobster@a923577c-bfe2-11de-93d8-795327aa6b9b>
2010-02-05 12:35:15 +0000

    trying to get a feel for the android platform

---
+++ mobileCloud/android/2_0/test-suite/mock_tests/src/main/java/org/openmobster/core/mobileCloud/test/mock/StorageTestDrive.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.openmobster.core.mobileCloud.test.mock;
+
+import org.openmobster.core.mobileCloud.android.testsuite.Test;
+
+/**
+ * @author openmobster
+ *
+ */
+public class StorageTestDrive extends Test
+{
+	public void runTest()
+	{
+		System.out.println("Executing......."+this);
+	}
+}
