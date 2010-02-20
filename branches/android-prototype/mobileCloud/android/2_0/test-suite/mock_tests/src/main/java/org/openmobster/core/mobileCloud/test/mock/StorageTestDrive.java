ecd1f345d2cbbe2d82f742f1f33e77004e095d96
openmobster <openmobster@a923577c-bfe2-11de-93d8-795327aa6b9b>
2010-02-20 21:55:24 +0000

    testdriving some more core Android services

--- branches/android-prototype/mobileCloud/android/2_0/test-suite/mock_tests/src/main/java/org/openmobster/core/mobileCloud/test/mock/StorageTestDrive.java
+++ branches/android-prototype/mobileCloud/android/2_0/test-suite/mock_tests/src/main/java/org/openmobster/core/mobileCloud/test/mock/StorageTestDrive.java
@@ -5,6 +5,8 @@ package org.openmobster.core.mobileCloud.test.mock;
 
 import org.openmobster.core.mobileCloud.android.testsuite.Test;
 
+import android.content.Context;
+
 /**
  * @author openmobster
  *
@@ -13,6 +15,27 @@ public class StorageTestDrive extends Test
 {
 	public void runTest()
 	{
-		System.out.println("Executing......."+this);
+		Database database = null;
+		try
+		{
+			Context context = (Context)this.getTestSuite().getContext().
+			getAttribute("android:context");
+		
+			database = new Database(context, "mobster.db",1);
+			database.open();
+		
+			//perform some queries
+			database.dump();
+		
+			database.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace(System.out);
+		}
+		finally
+		{
+			database.delete();
+		}
 	}
 }
---
+++ branches/android-prototype/mobileCloud/android/2_0/test-suite/mock_tests/src/main/java/org/openmobster/core/mobileCloud/test/mock/rpc/IBusHandler.aidl
@@ -0,0 +1,6 @@
+package org.openmobster.core.mobileCloud.test.mock.rpc;
+
+interface IBusHandler
+{
+	int getCounter();
+}
