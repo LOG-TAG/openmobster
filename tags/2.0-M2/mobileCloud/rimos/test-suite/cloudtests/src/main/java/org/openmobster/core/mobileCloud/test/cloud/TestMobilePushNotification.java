b1ed21c5d8d95e01111d3463e575e903a55996ff
openmobster <openmobster@a923577c-bfe2-11de-93d8-795327aa6b9b>
2010-03-02 05:07:54 +0000

    tagging 2.0-M2

---
+++ tags/2.0-M2/mobileCloud/rimos/test-suite/cloudtests/src/main/java/org/openmobster/core/mobileCloud/test/cloud/TestMobilePushNotification.java
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * OpenMobster                                                                *
+ * Copyright 2008, OpenMobster, and individual                                *
+ * contributors as indicated by the @authors tag. See the                     *
+ * copyright.txt in the distribution for a full listing of                    *
+ * individual contributors.                                                   *
+ *                                                                            *
+ * This is free software; you can redistribute it and/or modify it            *
+ * under the terms of the GNU Lesser General Public License as                *
+ * published by the Free Software Foundation; either version 2.1 of           *
+ * the License, or (at your option) any later version.                        *
+ *                                                                            *
+ * This software is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU           *
+ * Lesser General Public License for more details.                            *
+ *                                                                            *
+ * You should have received a copy of the GNU Lesser General Public           *
+ * License along with this software; if not, write to the Free                *
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA         *
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.                   *
+ ******************************************************************************/
+package org.openmobster.core.mobileCloud.test.cloud;
+
+import test.openmobster.core.mobileCloud.rimos.testsuite.AbstractAPITest;
+
+/**
+ * @author openmobster@gmail.com
+ *
+ */
+public final class TestMobilePushNotification extends AbstractAPITest 
+{
+	public void runTest()
+	{		
+		try
+		{
+			this.resetServerAdapter("setUp=TestOneWayServerSync/replace");
+			this.startOneWayServerSync();
+		}
+		catch(Exception e)
+		{
+			throw new RuntimeException(e.toString());
+		}
+	}
+}
