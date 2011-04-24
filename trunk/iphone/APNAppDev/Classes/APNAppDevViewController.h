492597e108a38026c6d0c4fadd5dc86aca06e024
openmobster@gmail.com <openmobster@gmail.com@a923577c-bfe2-11de-93d8-795327aa6b9b>
2011-04-24 17:16:33 +0000

    stabilizing the codebase before a snapshot release

---
+++ trunk/iphone/APNAppDev/Classes/APNAppDevViewController.h
@@ -0,0 +1,16 @@
+//
+//  APNAppDevViewController.h
+//  APNAppDev
+//
+//  Created by openmobster on 4/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APNAppDevViewController : UIViewController {
+
+}
+
+@end
+
