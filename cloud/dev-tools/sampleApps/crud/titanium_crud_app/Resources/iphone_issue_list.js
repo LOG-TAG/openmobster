af79ab7770b9fa98b6ce377cb92d5eaa31faac7b
openmobster@gmail.com <openmobster@gmail.com@a923577c-bfe2-11de-93d8-795327aa6b9b>
2011-06-13 23:37:05 +0000

    code backup (titanium integration)

---
+++ cloud/dev-tools/sampleApps/crud/titanium_crud_app/Resources/iphone_issue_list.js
@@ -0,0 +1,92 @@
+function loadWindow()
+{
+	//Cloud Module
+	var cloudModule = require('org.openmobster.cloud');
+	var sync = cloudModule.sync();
+	var channel = "crm_ticket_channel";
+	var oids = sync.readAll(channel);
+	
+	if(oids != null && oids != '[]')
+	{
+		oids = eval('('+oids+')');
+		
+		var data = new Array(oids.length);
+		for(var i=0; i<oids.length; i++)
+		{
+			var title = sync.getValue(channel,oids[i],"title");
+			var rowData = {title:title, hasChild:false, oid:oids[i]};
+			data[i] = rowData;
+		}
+		
+		// create table view
+		var tableview = Titanium.UI.createTableView({
+			data:data
+		});
+		
+		// add table view to the window
+		Titanium.UI.currentWindow.add(tableview);
+		
+		// create table view event listener
+		tableview.addEventListener('click', function(e)
+		{
+			if(e.rowData.oid)
+			{
+				var issueDialog = Titanium.UI.createAlertDialog({
+							title:"Issue",
+							message:e.rowData.title
+				});
+				
+				issueDialog.buttonNames = ['Update', 'Close','Delete'];
+				issueDialog.cancel = 1;
+				
+				Titanium.App.Properties.setString('oid',e.rowData.oid);
+				
+				issueDialog.show();
+				
+				//Add an EventHandler
+				issueDialog.addEventListener('click',function(e){
+					
+					if(e.index == 0)
+					{
+						var updateWindow = Titanium.UI.createWindow({
+							url:'iphone_update_issue.js',
+			    			titleImage:'images/appcelerator_small.png',
+			    			title:'Update Issue'
+						});
+						Titanium.UI.currentTab.open(updateWindow,{animated:true});
+					}
+					else if(e.index == 2)
+					{
+						//Delete this row
+					}
+				});
+			}
+		});
+	}
+	else
+	{
+		var notLoadedDialog = Titanium.UI.createAlertDialog({
+							title:"Synchronizing...",
+							message:"Data is not available yet"
+		});	
+		
+		notLoadedDialog.show();
+	}
+}
+
+
+Titanium.UI.currentWindow.addEventListener('open',function(){
+	loadWindow();
+});
+
+Ti.App.addEventListener('resume', function(e) {
+	var tabGroup = Ti.UI.currentWindow.tabGroup;
+	var tab = tabGroup.activeTab;
+	
+	if(tab.title == "Support Tickets")
+	{
+		loadWindow();
+	}
+});
+
+
---
+++ titanium-module/build/.apt_generated/org/openmobster/titanium/module/ExampleProxyBindingGen.java
@@ -0,0 +1,126 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2010 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+package org.openmobster.titanium.module;
+
+import org.appcelerator.kroll.KrollArgument;
+import org.appcelerator.kroll.KrollProxy;
+import org.appcelerator.kroll.KrollConverter;
+import org.appcelerator.kroll.KrollInvocation;
+import org.appcelerator.kroll.KrollMethod;
+import org.appcelerator.kroll.KrollModule;
+import org.appcelerator.kroll.KrollProxyBinding;
+import org.appcelerator.kroll.KrollModuleBinding;
+import org.appcelerator.kroll.KrollDynamicProperty;
+import org.appcelerator.kroll.KrollReflectionProperty;
+import org.appcelerator.kroll.KrollInjector;
+import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.kroll.KrollDefaultValueProvider;
+import org.appcelerator.kroll.util.KrollReflectionUtils;
+import org.appcelerator.kroll.util.KrollBindingUtils;
+import org.appcelerator.titanium.kroll.KrollBridge;
+import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.util.Log;
+
+import org.mozilla.javascript.Scriptable;
+
+import java.util.HashMap;
+
+import org.openmobster.titanium.module.ExampleProxy;
+
+
+/* WARNING: this code is generated for binding methods in Android */
+public class ExampleProxyBindingGen
+	extends org.appcelerator.kroll.KrollProxyBindingGen
+{
+	private static final String TAG = "ExampleProxyBindingGen";
+
+	private static final String METHOD_printMessage = "printMessage";
+		
+	public ExampleProxyBindingGen() {
+		super();
+		// Constants are pre-bound
+		
+		bindings.put(METHOD_printMessage, null);
+		
+	}
+
+	public void bindContextSpecific(KrollBridge bridge, KrollProxy proxy) {
+	}
+
+	@Override
+	public Object getBinding(String name) {
+		Object value = bindings.get(name);
+		if (value != null) { 
+			return value;
+		}
+
+
+
+
+
+
+		// Methods
+		if (name.equals(METHOD_printMessage)) {
+			KrollMethod printMessage_method = new KrollMethod(METHOD_printMessage) {
+				public Object invoke(KrollInvocation __invocation, Object[] __args) throws Exception
+				{
+	
+	KrollBindingUtils.assertRequiredArgs(__args, 1, METHOD_printMessage);
+		KrollArgument __message_argument = new KrollArgument("message");
+		java.lang.String message;
+			__message_argument.setOptional(false);
+			
+				message = (java.lang.String)
+					org.appcelerator.kroll.KrollConverter.getInstance().convertJavascript(__invocation, __args[0], java.lang.String.class);
+		__message_argument.setValue(message);
+		__invocation.addArgument(__message_argument);
+	
+	
+	
+	
+	((ExampleProxy) __invocation.getProxy()).printMessage(
+		message
+		);
+		return KrollProxy.UNDEFINED;
+				}
+			};
+			bindings.put(METHOD_printMessage, printMessage_method);
+			return printMessage_method;
+		}
+
+
+		return super.getBinding(name);
+	}
+
+	private static final String SHORT_API_NAME = "Example";
+	private static final String FULL_API_NAME = "TitaniumModule.Example";
+	private static final String ID = "org.openmobster.titanium.module.ExampleProxy";
+
+	public String getAPIName() {
+		return FULL_API_NAME;
+	}
+
+	public String getShortAPIName() {
+		return SHORT_API_NAME;
+	}
+
+	public String getId() {
+		return ID;
+	}
+
+	public KrollModule newInstance(TiContext context) {
+		return null;
+	}
+
+	public Class<? extends KrollProxy> getProxyClass() {
+		return ExampleProxy.class;
+	}
+
+	public boolean isModule() {
+		return false; 
+	}
+}
