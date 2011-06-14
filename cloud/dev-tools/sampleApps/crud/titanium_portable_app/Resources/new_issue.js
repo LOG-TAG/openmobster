85af879b615a6498e1f232430a47e528c635c297
openmobster@gmail.com <openmobster@gmail.com@a923577c-bfe2-11de-93d8-795327aa6b9b>
2011-06-14 01:32:53 +0000

    titanium portable app

---
+++ cloud/dev-tools/sampleApps/crud/titanium_portable_app/Resources/new_issue.js
@@ -0,0 +1,146 @@
+var win = Titanium.UI.currentWindow;
+
+//
+//  Title Field
+//
+var titleLabel = Titanium.UI.createLabel({
+	color:'#fff',
+	text:'Title',
+	top:10,
+	left:30,
+	width:100,
+	height:'auto'
+});
+
+win.add(titleLabel);
+
+var titleField = Titanium.UI.createTextField({
+	height:35,
+	top:35,
+	left:30,
+	width:250,
+	borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+});
+
+win.add(titleField);
+
+//Customer Picker
+var customerLabel = Titanium.UI.createLabel({
+	color:'#fff',
+	text:'Customer',
+	top:80,
+	left:30,
+	width:100,
+	height:'auto'
+});
+
+win.add(customerLabel);
+
+var customerPicker = Titanium.UI.createPicker({
+	top:105,
+	left:30,
+	width:'auto',
+	height:'auto'
+});
+var data = [];
+data[0]=Titanium.UI.createPickerRow({title:'Bananas'});
+data[1]=Titanium.UI.createPickerRow({title:'Strawberries'});
+data[2]=Titanium.UI.createPickerRow({title:'Mangos'});
+data[3]=Titanium.UI.createPickerRow({title:'Grapes'});
+customerPicker.add(data);
+
+win.add(customerPicker);
+
+//Specialist Picker
+var specialistLabel = Titanium.UI.createLabel({
+	color:'#fff',
+	text:'Specialist',
+	top:160,
+	left:30,
+	width:100,
+	height:'auto'
+});
+
+win.add(specialistLabel);
+
+var specialistPicker = Titanium.UI.createPicker({
+	top:185,
+	left:30,
+	width:'auto',
+	height:'auto'
+});
+var data = [];
+data[0]=Titanium.UI.createPickerRow({title:'Bananas'});
+data[1]=Titanium.UI.createPickerRow({title:'Strawberries'});
+data[2]=Titanium.UI.createPickerRow({title:'Mangos'});
+data[3]=Titanium.UI.createPickerRow({title:'Grapes'});
+specialistPicker.add(data);
+
+win.add(specialistPicker);
+
+
+//Comments
+var commentsLabel = Titanium.UI.createLabel({
+	color:'#fff',
+	text:'Comments',
+	top:235,
+	left:30,
+	width:100,
+	height:'auto'
+});
+
+win.add(commentsLabel);
+
+//Comments TextArea
+var comments = Titanium.UI.createTextArea({
+    height:70,
+    width:270,
+    top:260,
+    left:30,
+    font:{fontSize:20,fontFamily:'Marker Felt', fontWeight:'bold'},
+    color:'#888',
+    textAlign:'left',
+    appearance:Titanium.UI.KEYBOARD_APPEARANCE_ALERT,   
+    keyboardType:Titanium.UI.KEYBOARD_NUMBERS_PUNCTUATION,
+    returnKeyType:Titanium.UI.RETURNKEY_EMERGENCY_CALL,
+    borderWidth:2,
+    borderColor:'#bbb',
+    borderRadius:5
+});
+
+win.add(comments);
+
+var buttonView = Titanium.UI.createView({
+	layout:'horizontal',
+	height:'auto',
+	top:330,
+	width:100,
+	left:100
+	//backgroundColor:'#13386c'
+});
+
+var ok = Titanium.UI.createButton({
+	title: 'OK'
+});
+
+var cancel = Titanium.UI.createButton({
+    title:'Cancel'
+});
+
+buttonView.add(ok);
+buttonView.add(cancel);
+win.add(buttonView);
+
+//Event Handling
+ok.addEventListener('click',function(e){
+	var titleValue = titleField.value;
+	var customerValue = customerPicker.getSelectedRow(0);
+	
+	Ti.API.info("Title: "+titleValue);
+	Ti.API.info("Customer: "+customerValue);
+});
+
+cancel.addEventListener('click',function(e){
+	var tabGroup = Ti.UI.currentWindow.tabGroup;
+	tabGroup.setActiveTab(0);
+});
