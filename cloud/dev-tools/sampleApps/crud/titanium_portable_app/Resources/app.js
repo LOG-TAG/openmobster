2059586c6d737d83479320f7f3a43ea49ec096f0
openmobster <openmobster@a923577c-bfe2-11de-93d8-795327aa6b9b>
2011-06-29 20:52:08 +0000

    code stabilization

--- cloud/dev-tools/sampleApps/crud/titanium_portable_app/Resources/app.js
+++ cloud/dev-tools/sampleApps/crud/titanium_portable_app/Resources/app.js
@@ -1,59 +1,76 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
 
-var tabGroup = Titanium.UI.createTabGroup(
+function loadApp()
+{
+	// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+	Titanium.UI.setBackgroundColor('#000');
+	
+	var tabGroup = Titanium.UI.createTabGroup(
+		{
+			barColor:'#336699'
+		}
+	);
+	
+	var window1Url;
+	var window2Url;
+	
+	if (Titanium.Platform.name == 'iPhone OS')
 	{
-		barColor:'#336699'
+		window1Url = 'iphone_issue_list.js';
+		window2Url = 'iphone_new_issue.js';
 	}
-);
-
-var window1Url;
-var window2Url;
-
-if (Titanium.Platform.name == 'iPhone OS')
-{
-	window1Url = 'iphone_issue_list.js';
-	window2Url = 'iphone_new_issue.js';
+	else
+	{
+		window1Url = 'issue_list.js';
+		window2Url = 'new_issue.js';
+	}
+	
+	//create the tab1 window
+	var window1 = Titanium.UI.createWindow({
+		url:window1Url,
+	    titleImage:'images/appcelerator_small.png'
+	});
+	
+	var tab1 = Titanium.UI.createTab({
+		icon:'KS_nav_views.png',
+	    title:'Support Tickets',
+	    window:window1
+	});
+	
+	//create the tab2 window
+	var window2 = Titanium.UI.createWindow({
+		url:window2Url,
+	    titleImage:'images/appcelerator_small.png'
+	});
+	
+	var tab2 = Titanium.UI.createTab({
+		icon:'KS_nav_ui.png',
+	    title:'Report an Issue',
+	    window:window2
+	});
+	
+	//add the tabs
+	tabGroup.addTab(tab1);
+	tabGroup.addTab(tab2);
+	
+	//active tab
+	tabGroup.setActiveTab(0);
+	
+	
+	//open tab group with a transition animation
+	tabGroup.open({
+		transition:Titanium.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
+	});
 }
-else
+
+Ti.Android.currentActivity.addEventListener('resume', function(e) 
 {
-	window1Url = 'issue_list.js';
-	window2Url = 'new_issue.js';
-}
-
-//create the tab1 window
-var window1 = Titanium.UI.createWindow({
-	url:window1Url,
-    titleImage:'images/appcelerator_small.png'
-});
-
-var tab1 = Titanium.UI.createTab({
-	icon:'KS_nav_views.png',
-    title:'Support Tickets',
-    window:window1
-});
-
-//create the tab2 window
-var window2 = Titanium.UI.createWindow({
-	url:window2Url,
-    titleImage:'images/appcelerator_small.png'
-});
-
-var tab2 = Titanium.UI.createTab({
-	icon:'KS_nav_ui.png',
-    title:'Report an Issue',
-    window:window2
-});
-
-//add the tabs
-tabGroup.addTab(tab1);
-tabGroup.addTab(tab2);
-
-//active tab
-tabGroup.setActiveTab(0);
-
-
-//open tab group with a transition animation
-tabGroup.open({
-	transition:Titanium.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
+	//Cloud Module
+	var cloudModule = require('org.openmobster.cloud');
+	var sync = cloudModule.sync();
+	
+	//If Module is ready to take invocations
+	if(sync != null)
+	{
+		loadApp();
+	}
 });
