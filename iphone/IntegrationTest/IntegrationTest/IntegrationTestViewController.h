f068056e983a4a7fa2f5d0709528b2a7c6720cc2
openmobster@gmail.com <openmobster@gmail.com@a923577c-bfe2-11de-93d8-795327aa6b9b>
2011-08-19 02:14:35 +0000

    getting ready for 2.2-M4

---
+++ iphone/IntegrationTest/IntegrationTest/IntegrationTestViewController.h
@@ -0,0 +1,17 @@
+//
+//  IntegrationTestViewController.h
+//  IntegrationTest
+//
+//  Created by openmobster on 8/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UICommandDelegate.h"
+
+@interface IntegrationTestViewController : UIViewController<UITableViewDelegate,UITableViewDataSource,UICommandDelegate> 
+{
+    
+}
+
+@end
---
+++ iphone/IntegrationTest/IntegrationTest/SyncIntegrationTest.h
@@ -0,0 +1,19 @@
+//
+//  SyncIntegrationTest.h
+//  IntegrationTest
+//
+//  Created by openmobster on 8/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RemoteCommand.h"
+
+
+@interface SyncIntegrationTest : NSObject<RemoteCommand> 
+{
+    
+}
+
++(id)withInit;
+@end
