185802bcd1c4fe11574ad5d50f5a38df6d25a357
openmobster <openmobster@a923577c-bfe2-11de-93d8-795327aa6b9b>
2011-11-12 20:13:11 +0000

    Starting the TestSuite for Android Location Module

---
+++ cloud/android/test-suite/test-location/src/main/java/org/openmobster/core/mobileCloud/android/location/TestPayloadHandler.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) {2003,2011} {openmobster@gmail.com} {individual contributors as indicated by the @authors tag}.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openmobster.core.mobileCloud.android.location;
+
+import org.openmobster.core.mobileCloud.android.testsuite.Test;
+
+/**
+ *
+ * @author openmobster@gmail.com
+ */
+public class TestPayloadHandler extends Test
+{
+	@Override
+	public void runTest()
+	{
+	}
+}
