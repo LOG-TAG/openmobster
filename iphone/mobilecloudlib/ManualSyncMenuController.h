35a6865e1c5912cb7c10c05a68dbcafc4f1c7f26
openmobster@gmail.com <openmobster@gmail.com@a923577c-bfe2-11de-93d8-795327aa6b9b>
2012-08-28 19:18:22 +0000

    backing up some code

--- iphone/mobilecloudlib/ManualSyncMenuController.h
+++ iphone/mobilecloudlib/ManualSyncMenuController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 #import "Channel.h"
+#import "UICommandDelegate.h"
 
-@interface ManualSyncMenuController : UIViewController<UITableViewDataSource,UITableViewDelegate>
+@interface ManualSyncMenuController : UIViewController<UITableViewDataSource,UITableViewDelegate,UICommandDelegate>
 {
     @private
     Channel *selectedChannel;
---
+++ iphone/mobilecloudlib/SyncUtility.m
@@ -0,0 +1,45 @@
+//
+//  SyncUtility.m
+//  mobilecloudlib
+//
+//  Created by openmobster on 8/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SyncUtility.h"
+#import "SyncService.h"
+
+@implementation SyncUtility
+
++(id)withInit
+{
+	SyncUtility *syncUtility = (SyncUtility *)[[[SyncUtility alloc] init] autorelease];
+	return syncUtility;
+}
+
+-(void)resetChannel:(NSString *)channel
+{
+    @try 
+    {
+       SyncService *sync = [SyncService getInstance];
+       [sync performBootSync:channel :NO];
+    }
+    @catch (NSException *exception) 
+    {
+        @throw exception;
+    }
+}
+
+-(void)syncChannel:(NSString *)channel
+{
+    @try 
+    {
+        SyncService *sync = [SyncService getInstance];
+        [sync performTwoWaySync:channel :NO];
+    }
+    @catch (NSException *exception) 
+    {
+        @throw exception;
+    }
+}
+@end
