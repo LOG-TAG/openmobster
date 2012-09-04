091a9d2e03d8a2c173455bc3341ad3fa5afed642
openmobster@gmail.com <openmobster@gmail.com@a923577c-bfe2-11de-93d8-795327aa6b9b>
2012-09-04 21:25:58 +0000

    iOS Sample App with Sync+Push

---
+++ trunk/iphone/SampleApp/SampleApp/ViewController.m
@@ -0,0 +1,168 @@
+//
+//  ViewController.m
+//  SampleApp
+//
+//  Created by openmobster on 8/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import "CloudManager.h"
+#import "AppSession.h"
+#import "MobileBean.h"
+
+@implementation ViewController
+
+@synthesize beansTable;
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    //reload the data
+    [self.beansTable reloadData];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+//-------------------------------------------------------------------------------------------------------
+-(IBAction)launchCloudManager:(id)sender
+{
+    //Launch the CloudManager App
+    [CloudManager modalCloudManager:self];
+}
+
+-(void)setUpBeans
+{
+    //read the beans and populate the AppSession
+    AppSession *session = [AppSession getInstance];
+	NSArray *beans = [MobileBean readAll:@"webappsync_ticket_channel"];
+    
+    if(beans != nil)
+    {
+        [session setAttribute:@"beans" :beans];
+    } 
+}
+//--------------------------------------------------------------------------------------------------------
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section 
+{
+    [self setUpBeans];
+    
+	AppSession *session = [AppSession getInstance];
+	NSArray *beans = [session getAttribute:@"beans"];
+    
+    if(beans == nil)
+    {
+        return 0;
+    }
+    
+	int count = [beans count];
+	return count;
+}
+
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath 
+{
+    AppSession *session = [AppSession getInstance];
+	NSArray *beans = [session getAttribute:@"beans"];
+    
+	UITableViewCell *local = [tableView dequeueReusableCellWithIdentifier:@"show-beans"];
+	if(local == nil)
+	{
+		local = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"show-beans"];
+		local = [local autorelease];
+	}
+	
+	int index = indexPath.row;
+    
+    MobileBean *bean = [beans objectAtIndex:index];
+	
+	NSString *display = [bean getValue:@"title"];
+	local.textLabel.text = display;
+	
+	return local;
+}
+
+-(void) tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath 
+{
+}
+
+-(void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath 
+{
+    AppSession *session = [AppSession getInstance];
+	NSArray *beans = [session getAttribute:@"beans"];
+    
+	[tableView deselectRowAtIndexPath:indexPath animated:YES];
+	
+	int index = indexPath.row;
+    
+    MobileBean *bean = [beans objectAtIndex:index];
+    
+    //Show the ticket in a dialog box
+	NSString *title = [bean getValue:@"title"];
+	NSString *comment = [bean getValue:@"comment"];
+	
+	UIAlertView *dialog = [[UIAlertView alloc] initWithTitle:title
+													 message:comment 
+													delegate:self cancelButtonTitle:@"OK" 
+										   otherButtonTitles:nil];
+	
+	
+	dialog = [dialog autorelease];
+	[dialog show];
+}
+//--------------------------------------------------------------------------------------------------
+-(void)doViewAfter:(CommandContext *)callback
+{
+    [self.beansTable reloadData];
+}
+-(void)doViewError:(CommandContext *)callback
+{
+}
+-(void)doViewAppException:(CommandContext *)callback
+{
+}
+@end
