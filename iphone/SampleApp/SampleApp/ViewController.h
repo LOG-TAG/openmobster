1ea833464e359d6c913aeb0514324f4749acb87d
openmobster@gmail.com <openmobster@gmail.com@a923577c-bfe2-11de-93d8-795327aa6b9b>
2012-09-04 21:25:58 +0000

    iOS Sample App with Sync+Push

---
+++ iphone/SampleApp/SampleApp/ViewController.h
@@ -0,0 +1,24 @@
+//
+//  ViewController.h
+//  SampleApp
+//
+//  Created by openmobster on 8/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UICommandDelegate.h"
+
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate,UICommandDelegate>
+{
+    @private
+    IBOutlet UITableView *beansTable;
+}
+
+@property (nonatomic,strong) IBOutlet UITableView *beansTable;
+
+-(IBAction)launchCloudManager:(id)sender;
+
+-(void)setUpBeans;
+@end
