ee146fc5c689469526cc917f0052ec48670c0f6a
prashannagupta@gmail.com <prashannagupta@gmail.com@a923577c-bfe2-11de-93d8-795327aa6b9b>
2013-12-19 01:17:24 +0000

    MVC to non MVC

--- branches/2.4-SNAPSHOT/trunk/cloud/dev-tools/sampleApps/asyncserviceapp/app-android/src/main/java/org/async/service/app/cloud/app/command/AsyncGetDetails.java
+++ branches/2.4-SNAPSHOT/trunk/cloud/dev-tools/sampleApps/asyncserviceapp/app-android/src/main/java/org/async/service/app/cloud/app/command/AsyncGetDetails.java
@@ -12,100 +12,93 @@ import org.openmobster.android.api.rpc.MobileService;
 import org.openmobster.android.api.rpc.Request;
 import org.openmobster.android.api.rpc.Response;
 import org.openmobster.core.mobileCloud.android.errors.ErrorHandler;
-import org.openmobster.core.mobileCloud.android.service.Registry;
-import org.openmobster.core.mobileCloud.android_native.framework.ViewHelper;
-import org.openmobster.core.mobileCloud.api.ui.framework.Services;
 import org.openmobster.core.mobileCloud.api.ui.framework.command.AppException;
-import org.openmobster.core.mobileCloud.api.ui.framework.command.CommandContext;
-import org.openmobster.core.mobileCloud.api.ui.framework.command.AsyncCommand;
-
-import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 
 /**
- * The MVC AsyncCommand. The 'AsyncCommand' is used to perform actions asynchronously (Ajax from the web world). 
+ * The AsyncTask. The 'AsyncTask' is used to perform actions asynchronously (Ajax from the web world). 
  * 
- * This particular AsyncCommand, invokes the 'GetDetails' service in the Cloud and gets a fully populated Email instance for display.
+ * This particular AsyncTask, invokes the 'GetDetails' service in the Cloud and gets a fully populated Email instance for display.
  * 
  * @author openmobster@gmail.com
  *
  */
-public final class AsyncGetDetails implements AsyncCommand
-{
-	//Executes on the UI thread. All UI related operations are safe here. It is invoked to perform some pre-action UI related tasks.
-	public void doViewBefore(CommandContext commandContext)
-	{	
-		//Nothing to do
+
+public class AsyncGetDetails extends AsyncTask<Void,Void,Void>{
+
+	EmailBean email;
+	Context context;
+	ProgressDialog dialog = null;
+	Handler handler;
+	Message message;
+	
+	public AsyncGetDetails(Context context,Handler handler,EmailBean emailBean){
+		email=emailBean;
+		this.context=context;
+		this.handler = handler;	
+	}
+		
+	@Override
+	protected void onPostExecute(Void result){		
+		dialog.dismiss();
+		handler.sendMessage(message);
 	}
 
-	//This does not execute on the UI thread. When this method is invoked, the UI thread is freed up, so that its not frozen while the 
-	//information is being loaded from the Cloud
-	public void doAction(CommandContext commandContext) 
-	{
-		try
-		{
-			//Populate the Cloud Request
-			EmailBean email = (EmailBean)commandContext.getAttribute("email");
+	@Override
+	protected void onPreExecute(){
+		dialog = new ProgressDialog(context);		
+		dialog.setMessage("Please wait...");
+		dialog.setCancelable(false);
+		dialog.show();
+	}
+
+	@Override
+	protected Void doInBackground(Void... arg0){
+		//Populate the Cloud Request
+		try{
 			Request request = new Request("/asyncserviceapp/getdetails");	
 			request.setAttribute("oid", email.getOid());
-			
+		
 			Response response = new MobileService().invoke(request);
-			
+		
 			//Process the Cloud Response
 			String oid = response.getAttribute("oid");
 			String from = response.getAttribute("from");
 			String to = response.getAttribute("to");
 			String subject = response.getAttribute("subject");
 			String date = response.getAttribute("date");
-			
-			//Populate the Email bean with the information fetched from the Cloud
+		
 			email.setOid(oid);
 			email.setFrom(from);
 			email.setTo(to);
 			email.setSubject(subject);
 			email.setDate(date);
-		}
-		catch(Exception e)
-		{
+			
+			message= handler.obtainMessage();
+			message.what=1;
+			
+			StringBuilder buffer = new StringBuilder();
+			buffer.append("From: "+email.getFrom()+"\n");
+			buffer.append("To: "+email.getTo()+"\n");
+			buffer.append("Date: "+email.getDate()+"\n\n");
+			buffer.append("Subject: "+email.getSubject());
+			
+			message.obj=buffer.toString();
+			
+			
+		}catch(Exception e){
 			//throw an AppException. If this happens, the doViewError will be invoked to alert the user with an error message
 			AppException appe = new AppException();
 			appe.setMessage(e.getMessage());
 			
 			//Record this error in the Cloud Error Log
-			ErrorHandler.getInstance().handle(appe);
-			
+			ErrorHandler.getInstance().handle(appe);			
 			throw appe;
 		}
+		return null;
 	}	
-	
-	//Executes on the UI thread. All UI operations are safe. It is invoked after the doAction is executed without any errors.
-	//From an Ajax standpoint, consider this invocation as the UI callback
-	public void doViewAfter(CommandContext commandContext)
-	{
-		//Get the populated Email bean
-		EmailBean email = (EmailBean)commandContext.getAttribute("email");
-		
-		//Build the string to be displayed
-		StringBuilder buffer = new StringBuilder();
-		buffer.append("From: "+email.getFrom()+"\n");
-		buffer.append("To: "+email.getTo()+"\n");
-		buffer.append("Date: "+email.getDate()+"\n\n");
-		buffer.append("Subject: "+email.getSubject());
-		
-		//Display the details in a 'Dialog' box
-		Activity currentActivity = Services.getInstance().getCurrentActivity();
-		ViewHelper.getOkModal(currentActivity, "Email", 
-		buffer.toString()).
-		show();
-	}
-	
-	//Executes on the UI thread. All UI operations are safe. This method is invokes if there is an error during the doAction execution.
-	//From an Ajax standpoint, consider this invocation as a UI callback
-	public void doViewError(CommandContext commandContext)
-	{
-		//Shows an Error Alert
-		Activity currentActivity = Services.getInstance().getCurrentActivity();
-		ViewHelper.getOkModal(currentActivity, "App Error", 
-		commandContext.getAppException().getMessage()).
-		show();
-	}
 }
