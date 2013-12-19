f234973e3f56fd711379ddaf2e0d8b8c76eb7ce3
prashannagupta@gmail.com <prashannagupta@gmail.com@a923577c-bfe2-11de-93d8-795327aa6b9b>
2013-12-19 01:17:24 +0000

    MVC to non MVC

--- trunk/cloud/dev-tools/sampleApps/asyncserviceapp/app-android/src/main/java/org/async/service/app/cloud/app/screen/HomeScreen.java
+++ trunk/cloud/dev-tools/sampleApps/asyncserviceapp/app-android/src/main/java/org/async/service/app/cloud/app/screen/HomeScreen.java
@@ -8,15 +8,32 @@
 
 package org.async.service.app.cloud.app.screen;
 
-import java.lang.reflect.Field;
-
-import org.openmobster.core.mobileCloud.android.errors.ErrorHandler;
-import org.openmobster.core.mobileCloud.android.errors.SystemException;
-import org.openmobster.core.mobileCloud.api.ui.framework.Services;
-import org.openmobster.core.mobileCloud.api.ui.framework.command.CommandContext;
-import org.openmobster.core.mobileCloud.api.ui.framework.navigation.Screen;
-
+import java.util.List;
+import org.async.service.app.android.app.R;
+import org.async.service.app.cloud.app.command.AsyncGetDetails;
+import org.async.service.app.cloud.app.command.AsyncGetList;
+import org.async.service.app.cloud.app.command.EmailBean;
+import org.async.service.app.cloud.app.system.ActivationRequest;
+import org.async.service.app.cloud.app.system.MyBootstrapper;
+import org.openmobster.core.mobileCloud.android_native.framework.CloudService;
+import org.openmobster.core.mobileCloud.android_native.framework.ServiceException;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 /**
  * The Main Screen of the App. This is rendered when the App is launched. It is registered as the 'bootstrap' screen
@@ -24,53 +41,151 @@ import android.app.Activity;
  * 
  * @author openmobster@gmail.com
  */
-public class HomeScreen extends Screen
-{
-	private Integer screenId;
-	
+
+public class HomeScreen extends Activity{
+	ListView listView=null; 
 	@Override
-	public void render()
-	{
-		try
-		{
-			final Activity currentActivity = Services.getInstance().getCurrentActivity();
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+		listView=(ListView)findViewById(R.id.list);		
+		MyBootstrapper.getInstance().bootstrapUIContainerOnly(this);		
+	}
+	@Override
+	protected void onStart(){			
+		boolean isDeviceActivated = MyBootstrapper.getInstance().isDeviceActivated();
+		if(!isDeviceActivated)
+		{						
+			final AlertDialog builder=new AlertDialog.Builder(HomeScreen.this).create();
+			builder.setTitle("App Activation");
+			View view=LayoutInflater.from(this).inflate(R.layout.appactivation,null);
+			builder.setView(view);
+			final EditText serverip_t=(EditText)view.findViewById(R.id.serverip);
+			final EditText portno_t=(EditText)view.findViewById(R.id.portno);
+			final EditText emailid_t=(EditText)view.findViewById(R.id.emailid);
+			final EditText password_t=(EditText)view.findViewById(R.id.password);
 			
-			//Layout the Home Screen. The layout is specified in 'res/layout/home.xml'
-			String layoutClass = currentActivity.getPackageName()+".R$layout";
-			String home = "home";
-			Class clazz = Class.forName(layoutClass);
-			Field field = clazz.getField(home);
-			
-			this.screenId = field.getInt(clazz);						
-		}
-		catch(Exception e)
-		{
-			SystemException se = new SystemException(this.getClass().getName(), "render", new Object[]{
-				"Message:"+e.getMessage(),
-				"Exception:"+e.toString()
+			builder.setButton("Submit",new OnClickListener() {			
+				@Override
+				public void onClick(DialogInterface arg0, int arg1)
+				{
+					Handler handler=new Handler(){
+						@Override
+						public void handleMessage(Message msg)
+						{
+							int what=msg.what;
+							if(what==1){								
+								show();
+							}
+						}				
+					};
+					String serverip=serverip_t.getText().toString();
+					int portno=Integer.parseInt(portno_t.getText().toString());
+					String emailid=emailid_t.getText().toString();
+					String password=password_t.getText().toString();
+					ActivationRequest activationRequest=new ActivationRequest(serverip,portno,emailid,password);
+					new ToActivateDevice(HomeScreen.this,handler,activationRequest).execute();									
+				}
+			});		
+		
+			builder.setButton2("Cancel",new OnClickListener() {			
+				@Override
+				public void onClick(DialogInterface arg0, int arg1){
+					finish();
+				}
 			});
-			ErrorHandler.getInstance().handle(se);
-			throw se;
+			builder.show();				
+		}else{
+			show();
+		}		
+		super.onStart();
+	}
+	private void show(){
+		Handler handler=new Handler(){
+			@Override
+			public void handleMessage(Message msg){
+				int what=msg.what;
+				if(what==1){
+					final List<EmailBean> emails = (List<EmailBean>)msg.obj;
+					final String[] ui = new String[emails.size()];
+					for(int i=0,size=ui.length;i<size;i++)
+					{
+						EmailBean email = emails.get(i);
+						ui[i] = email.getSubject();
+					}
+					listView.setAdapter(new ArrayAdapter(HomeScreen.this,android.R.layout.simple_list_item_1,ui));
+					listView.setOnItemClickListener(new OnItemClickListener() {
+						@Override
+						public void onItemClick(AdapterView<?> arg0, View arg1,	int position, long arg3){
+							EmailBean emailBean=emails.get(position);
+							Handler handler=new Handler(){
+								public void handleMessage(Message msg) {
+									int what=msg.what;
+									if(what==1){
+										String res=msg.obj.toString();
+										
+										AlertDialog.Builder builder=new AlertDialog.Builder(HomeScreen.this);										
+										builder.setTitle("Detail");
+										builder.setMessage(res);
+										builder.setPositiveButton("Ok",null);
+										builder.show();
+										
+									}									
+								}								
+							};
+							new AsyncGetDetails(HomeScreen.this,handler, emailBean).execute();							
+						}						
+					});					
+				}
+			}
+		};		
+		new AsyncGetList(HomeScreen.this, handler).execute();		
+		
+	}
+	class ToActivateDevice extends AsyncTask<Void,Void,Void>{
+
+		Context context;
+		ProgressDialog dialog = null;
+		Handler handler;
+		Message message;
+		ActivationRequest activationRequest;		
+		
+		public ToActivateDevice(Context context,Handler handler,ActivationRequest activationRequest){
+			this.context=context;
+			this.handler = handler;
+			this.activationRequest=activationRequest;
 		}
-	}
-	
-	@Override
-	public Object getContentPane()
-	{
-		return this.screenId;
-	}
-	
-	@Override
-	public void postRender()
-	{
-		//Asynchronously get a list of emails from the Cloud
-		CommandContext commandContext = new CommandContext();
 		
-		//'/asyncserviceapp/getlist' indicates the URI of the AsyncCommand instance that will be invoked
-		//This is registered in the 'resources/moblet-app/moblet-app.xml' file
-		commandContext.setTarget("/asyncserviceapp/getlist");
+		@Override
+		protected void onPostExecute(Void result)
+		{
+			dialog.dismiss();
+			handler.sendMessage(message);
+		}
+
+		@Override
+		protected void onPreExecute()
+		{
+			dialog = new ProgressDialog(context);		
+			dialog.setMessage("Please wait...");
+			dialog.setCancelable(false);
+			dialog.show();	
+		}
 		
-		//Making the invocation
-		Services.getInstance().getCommandService().execute(commandContext);
+		@Override
+		protected Void doInBackground(Void... arg0){			 
+			try
+			{
+				CloudService.getInstance().activateDevice(activationRequest.getServerIP(),activationRequest.getPortNo(),activationRequest.getEmailId(),activationRequest.getPassword());
+				MyBootstrapper.getInstance().bootstrapUIContainer(HomeScreen.this);
+			}
+			catch(ServiceException se)
+			{
+				
+			}
+			message=handler.obtainMessage();
+			message.what=1;
+			return null;
+		}		
 	}
 }
